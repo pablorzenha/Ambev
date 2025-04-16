3676eb5e09fa6b6276d4a8a09a9d65fdf6258975
Pablo Araujo <pablo.araujo@globalhitss.com.br>
2025-04-16 18:39:25 -0300

    feat(update handler unit test): create first logic to update handler

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/UseCases/Sales/TestData/UpdateSaleHandlerTestData.cs
@@ -0,0 +1,31 @@
+﻿using Ambev.DeveloperEvaluation.Application.UseCases.Sales.UpdateSale;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application.UseCases.Sales.TestData
+{
+    public static class UpdateSaleHandlerTestData
+    {
+
+        private static readonly Faker<UpdateSaleCommand> GetSaleCommandFaker = new Faker<UpdateSaleCommand>()
+            .RuleFor(c => c.Id, f => Guid.NewGuid())
+            .RuleFor(c => c.SaleNumber, f => $"VENDA{f.Random.AlphaNumeric(5)}")
+            .RuleFor(c => c.Date, f => f.Date.Past())
+            .RuleFor(c => c.Status, f => SaleStatus.NotCancelled)
+            .RuleFor(c => c.CustomerId, f => Guid.NewGuid())
+            .RuleFor(c => c.BranchId, f => Guid.NewGuid())
+            .RuleFor(c => c.Items, f => new List<UpdateSaleItemCommand>
+            {
+                    new UpdateSaleItemCommand
+                    {
+                        ProductId = Guid.NewGuid(),
+                        Quantity = f.Random.Number(1, 10),
+                        UnitPrice = f.Finance.Amount(10, 100)
+                    }
+            });
+        /// <summary>
+        /// Generates a valid UpdateSaleCommand.
+        /// </summary>
+        public static UpdateSaleCommand GenerateValidCommand() => GetSaleCommandFaker.Generate();
+    }
+}
--- template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/UseCases/Sales/UpdateSaleHandlerTests.cs
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/UseCases/Sales/UpdateSaleHandlerTests.cs
@@ -1,8 +1,10 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Interfaces;
 using Ambev.DeveloperEvaluation.Application.UseCases.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.UseCases.Sales.UpdateSale;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Unit.Application.UseCases.Sales.TestData;
 using AutoMapper;
+using Bogus;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
@@ -10,26 +12,26 @@ using Xunit;
 
 namespace Ambev.DeveloperEvaluation.Unit.Application.UseCases.Sales
 {
-    public class CreateSaleHandlerTests
+    public class UpdateSaleHandlerTests
     {
         private readonly IMapper _mapper;
         private readonly ISaleService _saleService;
         private readonly ILogger<CreateSaleHandler> _logger;
-        private readonly CreateSaleHandler _handler;
+        private readonly UpdateSaleHandler _handler;
 
-        public CreateSaleHandlerTests()
+        public UpdateSaleHandlerTests()
         {
             _mapper = Substitute.For<IMapper>();
             _saleService = Substitute.For<ISaleService>();
             _logger = Substitute.For<ILogger<CreateSaleHandler>>();
-            _handler = new CreateSaleHandler(_mapper, _saleService, _logger);
+            _handler = new UpdateSaleHandler(_saleService, _mapper);
         }
 
         [Fact]
         public async Task Handle_ValidCommand_ReturnsMappedResult()
         {
             // Arrange
-            var command = CreateSaleHandlerTestData.GenerateValidCommand();
+            var command = UpdateSaleHandlerTestData.GenerateValidCommand();
             var sale = new Sale
             {
                 Id = Guid.NewGuid(),
@@ -47,14 +49,25 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.UseCases.Sales
                 TotalAmount = command.Items.Sum(i => i.Quantity * i.UnitPrice)
             };
 
-            var expectedResult = new CreateSaleResult
+            var expectedResult = new UpdateSaleResult
             {
                 Id = sale.Id,
-                TotalAmount = sale.TotalAmount
+                SaleNumber = command.SaleNumber,
+                Date = new Faker().Date.Past(),
+                Status = command.Status,
+                CustomerId = command.CustomerId.ToString(),
+                BranchId = command.BranchId.ToString(),
+                Items = command.Items.Select(i => new UpdateSaleItemResult
+                {
+                    ProductId = i.ProductId,
+                    Quantity = i.Quantity,
+                    UnitPrice = i.UnitPrice
+                }).ToList(),
+                TotalAmount = command.Items.Sum(i => i.Quantity * i.UnitPrice)
             };
 
-            _saleService.CreateAsync(command, Arg.Any<CancellationToken>()).Returns(sale);
-            _mapper.Map<CreateSaleResult>(sale).Returns(expectedResult);
+            _saleService.UpdateAsync(command, Arg.Any<CancellationToken>()).Returns(sale);
+            _mapper.Map<UpdateSaleResult>(sale).Returns(expectedResult);
 
             // Act
             var result = await _handler.Handle(command, CancellationToken.None);
@@ -62,7 +75,7 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.UseCases.Sales
             // Assert
             result.Should().NotBeNull();
             result.Id.Should().Be(sale.Id);
-            result.TotalAmount.Should().Be(sale.TotalAmount);
+            result.Date.Should().NotBe(sale.Date);
         }
     }
 }
